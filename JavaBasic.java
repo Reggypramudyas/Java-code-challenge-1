e27629e2601e37d96bde7a183b5581194dfe0022
Reggypramudyas <91525358+Reggypramudyas@users.noreply.github.com>
2021-10-12 16:02:54 +0700

    Add files via upload

---
+++ JavaBasic.java
@@ -0,0 +1,27 @@
+public class JavaBasic {
+
+    // function is a method
+    public static void main(String[] args) {
+        String first_name, last_name, phone_number, address, email, campusName, subject;
+        int age;
+
+        first_name = "Reggy Pramudya";
+        last_name = "Sulaeman";
+        age = 19;
+        phone_number = "089502401144";
+        address = "Bandung";
+        email = "Reggypsulaeman@gmail.com";
+        campusName = "Politeknik Negeri Bandung";
+        subject = "D4-Asset Management";
+
+        System.out.println(first_name);
+        System.out.println(last_name);
+        System.out.println(age);
+        System.out.println(phone_number);
+        System.out.println(address);
+        System.out.println(email);
+        System.out.println(campusName);
+        System.out.println(subject);
+
+    }
+}
